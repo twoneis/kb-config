398e234e50b8731a98e1d76e2a1e345eb40bcfc1
twoneis <sanjay29@sapura.de>
2024-05-22 18:50:32 +0200

    added rules.mk

--- build.fish
+++ build.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
 cp keymap.c qmk_firmware/keyboards/splitkb/aurora/sweep/keymaps/twoneis/
+cp rules.mk qmk_firmware/keyboards/splitkb/aurora/sweep/
 qmk compile
 mv qmk_firmware/splitkb_aurora_sweep_rev1_twoneis_liatris.uf2 keymap.uf2
