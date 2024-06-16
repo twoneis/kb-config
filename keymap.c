fcb8a62b62a564b1f52f002dec2a38aaaaca05a5
twoneis <sanjay29@sapura.de>
2024-06-16 16:25:34 +0200

    gaming layers

--- keymap.c
+++ keymap.c
@@ -12,14 +12,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[1] = LAYOUT(
 		KC_VOLU, KC_VOLD, KC_BRIU, KC_BRID, CW_TOGG,                 KC_MINS, KC_1, KC_2, KC_3, KC_BSPC,
 		KC_BSLS, KC_SLSH, SC_LCPO , SC_RSPC, KC_QUOT,                KC_EQL, MT(MOD_RSFT, KC_4), MT(MOD_RCTL, KC_5), KC_6, KC_0,
-		KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_LALT,                       KC_NO, KC_7, KC_8, KC_9, KC_ENT,
-		KC_LGUI, KC_RALT,                                              TO(2), TO(0)
+		KC_NO, KC_NO, KC_LBRC, KC_RBRC, KC_LALT,                     KC_NO, KC_7, KC_8, KC_9, KC_ENT,
+		KC_LGUI, KC_RALT,                                            TO(2), TO(0)
 	),
 	[2] = LAYOUT(
 		KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                           KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC,
 		KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,                          KC_NO, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT,
 		KC_F11, KC_F12, KC_F13, KC_F14, KC_F15,                      KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT,
-		KC_NO, KC_NO,                                                KC_NO, TO(0)
+		KC_NO, KC_NO,                                                TO(3), TO(0)
+	),
+	[3] = LAYOUT(
+		KC_ESC, KC_Q, KC_W, KC_R, KC_T,                              KC_NO, KC_1, KC_2, KC_3, KC_BSPC,
+		KC_LSFT, KC_A, KC_S, KC_D, KC_F,                             KC_NO, KC_4, KC_5, KC_6, KC_0,
+		KC_LCTL, KC_X, KC_Z, KC_V, KC_B,                             KC_NO, KC_7, KC_8, KC_9, KC_ENT,
+		KC_TAB, KC_SPC,                                              TO(4), TO(0)
+	),
+	[4] = LAYOUT(
+		KC_ESC, KC_1, KC_2, KC_3, KC_4,                             KC_NO, KC_NO, KC_NO, KC_NO, KC_BSPC,
+		KC_Q, KC_W, KC_R, KC_T, KC_Y,                               KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+		KC_LSFT, KC_LCTL, KC_F, KC_F, KC_B,                         KC_NO, KC_NO, KC_NO, KC_NO, KC_ENT,
+		KC_TAB, KC_SPC,                                             KC_NO, TO(0)
 	)
 	// clang-format on
 };
@@ -46,6 +58,15 @@ layer_state_set_user(layer_state_t state)
 	case 2:
 		rgblight_enable_noeeprom();
 		rgblight_sethsv_noeeprom(HSV_YELLOW);
+		break;
+	case 3:
+		rgblight_enable_noeeprom();
+		rgblight_sethsv_noeeprom(HSV_PURPLE);
+		break;
+	case 4:
+		rgblight_enable_noeeprom();
+		rgblight_sethsv_noeeprom(HSV_CORAL);
+		break;
 	default:
 		rgblight_disable_noeeprom();
 		break;
